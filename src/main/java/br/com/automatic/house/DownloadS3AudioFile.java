06126ac2490b1b569eb41a5bdb9b0f7abe0851e4
Fellipe Oliveira <fellipe.oliveira@oruspay.com.br>
2017-08-16 09:09:43 -0300

    speech to text with google api

--- src/main/java/br/com/automatic/house/DownloadS3AudioFile.java
+++ src/main/java/br/com/automatic/house/DownloadS3AudioFile.java
@@ -1,6 +1,10 @@
 package br.com.automatic.house;
 
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
@@ -9,13 +13,21 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.util.IOUtils;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.protobuf.ByteString;
 
 public class DownloadS3AudioFile {
 
 	public static void download(){
 		AWSCredentials credentials = null;
         try {
-            credentials = new ProfileCredentialsProvider("default").getCredentials();
+            credentials = new ProfileCredentialsProvider("fellipe.oliveira").getCredentials();
         } catch (Exception e) {
             throw new AmazonClientException(
                     "Cannot load the credentials from the credential profiles file. " +
@@ -26,13 +38,55 @@ public class DownloadS3AudioFile {
         AmazonS3 s3 = new AmazonS3Client(credentials);
 		com.amazonaws.services.s3.model.S3Object object = s3.getObject(new GetObjectRequest(Constants.BUCKET_NAME, "recording.3gp"));
 		try {
-			IOUtils.copy(object.getObjectContent(), new FileOutputStream("C:/Users/Fellipe/Desktop/recording.3gp"));
+			IOUtils.copy(object.getObjectContent(), new FileOutputStream("/home/fellipe/Projetos/recording.3gp"));
+			transformToText("/home/fellipe/Projetos/recording.3gp");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}	
 	
+	public static void transformToText(String caminho){
+		try{
+			// Instantiates a client
+		    SpeechClient speech = SpeechClient.create();
+
+		    // The path to the audio file to transcribe
+		    String fileName = caminho;
+
+		    // Reads the audio file into memory
+		    Path path = Paths.get(fileName);
+		    byte[] data = Files.readAllBytes(path);
+		    ByteString audioBytes = ByteString.copyFrom(data);
+
+		    // Builds the sync recognize request
+		    RecognitionConfig config = RecognitionConfig.newBuilder()
+		        .setEncoding(AudioEncoding.LINEAR16)
+		        .setSampleRateHertz(8000)
+		        .setLanguageCode("pt-BR")
+		        .build();
+		    RecognitionAudio audio = RecognitionAudio.newBuilder()
+		        .setContent(audioBytes)
+		        .build();
+
+		    // Performs speech recognition on the audio file
+		    RecognizeResponse response = speech.recognize(config, audio);
+		    List<SpeechRecognitionResult> results = response.getResultsList();
+
+		    for (SpeechRecognitionResult result: results) {
+		      List<SpeechRecognitionAlternative> alternatives = result.getAlternativesList();
+		      for (SpeechRecognitionAlternative alternative: alternatives) {
+		        System.out.printf("Transcription: %s%n", alternative.getTranscript());
+		      }
+		    }
+		    speech.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
 	public static void main(String[] args) {
-		download();
+		//download();
+		transformToText("/home/fellipe/Projetos/recording.wav");
 	}
 }
